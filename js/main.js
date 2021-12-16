64320cf52989e6e0214e8d0b81d93a0aa565c640
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-12-17 00:53:17 +0700

    logo in the middle

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 function init() {
+    const app = new PIXI.Application({
+        width: 800,
+        height: 600,
+        resolution: window.devicePixelRatio || 1
+    });
+
+    document.body.appendChild(app.view);
+
+    const logo_texture = PIXI.Texture.from('img/logo.png');
+
+    const logo = new PIXI.Sprite(logo_texture);
+    logo.anchor.set(.5);
+    app.stage.addChild(logo);
+
+    logo.x = app.screen.width / 2;
+    logo.y = app.screen.height / 2;
 }
