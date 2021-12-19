ed54567f2b74cdcc406d3a2cda79c0200bafba33
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-12-19 20:27:34 +0700

    added a simple tweening module

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let app;
 let layout;
+let tweening;
 
 function setup() {
     const app = new PIXI.Application({
@@ -16,17 +17,35 @@ function setup() {
 function init() {
     app = setup();
     layout = new Layout(app);
+    tweening = new Tweening(app.ticker);
 
     const logo_texture = PIXI.Texture.from('img/logo.png');
     const background_texture = PIXI.Texture.from('img/background.png');
 
     const logo = new PIXI.Sprite(logo_texture);
-    const background = new PIXI.Sprite(background_texture);
+    logo.anchor.set(.5);
 
+    const background = new PIXI.Sprite(background_texture);
     background.anchor.set(.5);
 
     app.stage.addChild(background, logo);
 
     layout.add(background).pin(.5, .5).fit(true);
-    layout.add(logo).pin(0, 0).offset(10, 10);
+    layout.add(logo).pin(.5, .3);
+
+    logo.scale.x = 0;
+    tweening.add(new Tween(
+        1,
+        () => logo.scale.x,
+        x => logo.scale.x = x,
+        1000,
+        backout(10),
+        () => console.log('yay!')
+    ));
+
+    //let acc = 0;
+    //app.ticker.add(delta => {
+        //acc += delta / 2 * Math.PI;
+        //logo.scale.x = Math.cos(acc / 100);
+    //});
 }
---
+++ js/tween.js
@@ -0,0 +1,59 @@
+class Tweening {
+    constructor(ticker) {
+        this.tweens = [];
+        ticker.add(delta => this.tick(delta));
+    }
+
+    tick(_delta) {
+        const now = Date.now();
+
+        for (let i = this.tweens.length - 1; i >= 0; --i) {
+            const tween = this.tweens[i];
+            tween.tick(now);
+            if (tween.complete) {
+                this.tweens.splice(i, 1);
+            }
+        }
+    }
+
+    add(tween) {
+        if (!(tween instanceof Tween)) {
+            throw ValueError('tween must be an instance of Tween');
+        }
+        this.tweens.push(tween);
+    }
+}
+
+class Tween {
+    constructor(to, getter, setter, duration, easing, onComplete = null) {
+        this.to = to;
+        this.getter = getter;
+        this.setter = setter;
+        this.duration = duration;
+        this.easing = easing;
+        this.onComplete = onComplete;
+
+        this.from = this.getter();
+        this.start = Date.now();
+        this.complete = false;
+    }
+
+    tick(now) {
+        const phase = Math.min(1, (now - this.start) / this.duration);
+        this.setter(lerp(this.from, this.to, this.easing(phase)));
+
+        if (phase === 1) {
+            this.onComplete?.();
+            this.complete = true;
+            return;
+        }
+    }
+}
+
+function lerp(a, b, t) {
+    return a * (1 - t) + b * t;
+}
+
+function backout(amount) {
+    return (t) => (--t * t * ((amount + 1) * t + amount) + 1);
+}
